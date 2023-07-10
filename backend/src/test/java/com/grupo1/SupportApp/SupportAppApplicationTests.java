c601f302de96fb374aab038f94223b5af93faabc
PabloEsteban87 <pablosebas87@gmail.com>
2023-07-10 09:34:12 +0200

    first commit

---
+++ backend/src/test/java/com/grupo1/SupportApp/SupportAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo1.SupportApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupportAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
