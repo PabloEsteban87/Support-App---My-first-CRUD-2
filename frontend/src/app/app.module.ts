ee98565db611fedad1270ef1e03ffca8458833ae
PabloEsteban87 <pablosebas87@gmail.com>
2023-07-12 11:11:50 +0200

    folders 2

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,9 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { MainComponent } from './shared/main/main.component';
-import { RequestlistComponent } from './components/requestlist/requestlist.component';
-import { RequestsupportComponent } from './components/requestsupport/requestsupport.component';
-import { RequesteditionComponent } from './components/requestedition/requestedition.component';
+import { RequestlistComponent } from './support-app/components/requestlist/requestlist.component';
+import { RequestsupportComponent } from './support-app/components/requestsupport/requestsupport.component';
+import { RequesteditionComponent } from './support-app/components/requestedition/requestedition.component';
+import { ModelsComponent } from './support-app/models/models.component';
+import { ServicesComponent } from './support-app/services/services.component';
+import { ViewsComponent } from './support-app/views/views.component';
+import { EnvironmentsComponent } from '../environments/environments.component';
+
 
 @NgModule({
   declarations: [
@@ -16,7 +21,11 @@ import { RequesteditionComponent } from './components/requestedition/requestedit
     MainComponent,
     RequestlistComponent,
     RequestsupportComponent,
-    RequesteditionComponent
+    RequesteditionComponent,
+    ModelsComponent,
+    ServicesComponent,
+    ViewsComponent,
+    EnvironmentsComponent,
   ],
   imports: [
     BrowserModule,
