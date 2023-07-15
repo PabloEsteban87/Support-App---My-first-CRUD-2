af0457554710b092470b2f73e941fa32d930b45b
PabloEsteban87 <pablosebas87@gmail.com>
2023-07-16 00:52:30 +0200

    pruebas

--- frontend/src/app/support-app/components/requestsupportForm/requestsupport.component.ts
+++ frontend/src/app/support-app/components/requestsupportForm/requestsupport.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-requestsupport',
   templateUrl: './requestsupport.component.html',
   styleUrls: ['./requestsupport.component.scss']
 })
-export class RequestsupportComponent {
+export class RequestsupportComponent implements OnInit{
+
+  constructor(/* private http: HttpClient */){}
+
+  AddSupportForm!: ;
+
+ /*  AddSupportForm = new FormGroup({
+    name : new FormControl(""),
+    date : new FormControl(""),
+    topic : new FormControl(""),
+    description : new FormControl("")
+  }); */
+
+  ngOnInit(): void {}
+  
+  onSubmitForm(){
+    /* this.addSupport(); */
+    console.log("hola");
+    console.log(this.AddSupportForm.value);
+  }
+
+/*   addSupport(){
+    this.http.post<any[]>('http://localhost:8000/support', {title: 'Angular post request'})
+    .subscribe(createSupport => 
+      {
+         createSupport.push(this.AddSupportForm.value); 
+          console.log(this.AddSupportForm.value);
+          console.log(createSupport);
+      });
+  }  */
 
 }
